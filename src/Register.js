a93b6e21aaa537d68b56ea4f07053c8042a45f8f
Fernando <luisfernandosalasg@gmail.com>
2022-04-20 15:49:51 -0500

    Add form validation

--- src/Register.js
+++ src/Register.js
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Container from "@mui/material/Container";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Link from "@mui/material/Link";
 import Copyright from "./Copyright";
+import {
+	Container,
+	CssBaseline,
+	Box,
+	Avatar,
+	Typography,
+	Grid,
+	TextField,
+	Button,
+	Link,
+} from "@mui/material";
 
 const theme = createTheme();
 
 const Register = () => {
+	const [values, setValues] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+	});
+
+	const [submitted, setSubmitted] = useState(false);
+	const [valid, setValid] = useState(false);
+	const [message, setMessage] = useState(true);
+
+	const handleFirstName = (e) => {
+		setValues({ ...values, firstName: e.target.value });
+	};
+
+	const handleLastName = (e) => {
+		setValues({ ...values, lastName: e.target.value });
+	};
+
+	const handleEmail = (e) => {
+		setValues({ ...values, email: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (values.firstName && values.lastName && values.email) {
+			setValid(true);
+			setMessage(false);
+		}
+		setSubmitted(true);
+		setValues({ firstName: "", lastName: "", email: "" });
+		setTimeout(() => {
+			setSubmitted(false);
+		}, 1500);
+	};
+
 	return (
 		<ThemeProvider theme={theme}>
 			<Container component="main" maxWidth="xs">
@@ -35,8 +72,14 @@ const Register = () => {
 					</Typography>
 					<Box component="form" noValidate sx={{ mt: 3 }}>
 						<Grid container spacing={2}>
+							{valid && submitted && (
+								<Typography component="h6" sx={{ color: "success.main" }}>
+									Succes! Thank you for registering
+								</Typography>
+							)}
 							<Grid item xs={12} sm={6}>
 								<TextField
+									value={values.firstName}
 									autoComplete="given-name"
 									name="firstName"
 									required
@@ -44,10 +87,17 @@ const Register = () => {
 									id="firstName"
 									label="First Name"
 									autoFocus
+									onChange={handleFirstName}
 								/>
+								{submitted && !values.firstName && message && (
+									<Typography component="h6" sx={{ color: "error.main" }}>
+										Please enter a first name
+									</Typography>
+								)}
 							</Grid>
 							<Grid item xs={12} sm={6}>
 								<TextField
+									value={values.lastName}
 									autoComplete="family-name"
 									name="lastName"
 									required
@@ -55,17 +105,30 @@ const Register = () => {
 									id="lastName"
 									label="Last Name"
 									autoFocus
+									onChange={handleLastName}
 								/>
+								{submitted && !values.lastName && message && (
+									<Typography component="h6" sx={{ color: "error.main" }}>
+										Please enter a last name
+									</Typography>
+								)}
 							</Grid>
 							<Grid item xs={12}>
 								<TextField
+									value={values.email}
 									required
 									fullWidth
 									id="email"
 									label="Email Address"
 									name="email"
 									autoComplete="email"
+									onChange={handleEmail}
 								/>
+								{submitted && !values.email && message && (
+									<Typography component="h6" sx={{ color: "error.main" }}>
+										Please enter an email
+									</Typography>
+								)}
 							</Grid>
 						</Grid>
 						<Button
@@ -73,6 +136,7 @@ const Register = () => {
 							fullWidth
 							variant="contained"
 							sx={{ mt: 3, mb: 2 }}
+							onClick={handleSubmit}
 						>
 							Register
 						</Button>
